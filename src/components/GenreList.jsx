8b05395bf63343051e5d82d6f7f1fcb3a24e853d
nvsirkhvn404 <nvsirkhvn.study@gmail.com>
2025-11-21 00:10:02 +0530

    Clean up the genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -9,27 +9,50 @@ export default function GenreList({ selectedGenre, onSelectGenre }) {
 
 	if (error) return null;
 
-	if (isLoading) return (
-		<div className="bg-card border-t hidden sm:flex sm:flex-col p-3">
-			{skeletons.map((skeleton) => (
-				<GenreListSkeleton key={skeleton}/>
-			))}
-		</div>
-	);
+	if (isLoading)
+		return (
+			<div className="bg-card border-t hidden sm:flex sm:flex-col p-3">
+				{skeletons.map((skeleton) => (
+					<GenreListSkeleton key={skeleton} />
+				))}
+			</div>
+		);
 
 	return (
-		<ul className="bg-card border-r-2 hidden sm:flex sm:flex-col p-3">
-			{data.map((genre) => (
-				<li key={genre.id} className={genre.id === selectedGenre?.id ? "bg-white rounded-xl px-4 p-2 m-2 " : "bg-stone-700 rounded-xl px-4 p-2 m-2" }>
-					<div className="flex items-center">
-						<img
-							src={getCroppedImageUrl(genre.image_background)}
-							className="size-8 rounded-xl"
-						/>
-						<Button  onClick={() => onSelectGenre(genre)} variant="link" className={genre.id === selectedGenre?.id ? "font-bold text-stone-700" : "font-bold"}>{genre.name}</Button>
-					</div>
-				</li>
-			))}
-		</ul>
+		<div className="hidden sm:flex flex-col bg-card border-r-2">
+			<h2 className="text-2xl sm:text-3xl md:text-4xl m-4 font-bold ">
+				Genres
+			</h2>
+			<ul className="space-y-4 mx-4">
+				{data.map((genre) => (
+					<li
+						key={genre.id}
+						className={
+							genre.id === selectedGenre?.id
+								? "bg-[#e5e5e5] rounded-xl px-4 py-2"
+								: "bg-stone-700 rounded-xl px-4 p-2"
+						}
+					>
+						<div className="flex items-center">
+							<img
+								src={getCroppedImageUrl(genre.image_background)}
+								className="size-10 rounded-lg object-cover"
+							/>
+							<Button
+								onClick={() => onSelectGenre(genre)}
+								variant="link"
+								className={
+									genre.id === selectedGenre?.id
+										? "font-bold text-stone-700 text-lg"
+										: "font-bold"
+								}
+							>
+								{genre.name}
+							</Button>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
